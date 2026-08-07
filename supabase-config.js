9382f75ac3e5c69e6f7a2535121f1ab86cf6def1
comzorlimpiezas01-dot <comzorlimpiezas.01@gmail.com>
2026-08-06 19:03:56 -0500

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,3 @@
 // DOMZOR Supabase Configuration
 window.DOMZOR_SUPABASE_URL = "https://yvnblhvyqjlqwbrnublg.supabase.co";
-window.DOMZOR_SUPABASE_ANON_KEY = sb_publishable_Zg4GulB7VDis2rKeio5Wqg_jmq_SPzp
+window.DOMZOR_SUPABASE_ANON_KEY = "sb_publishable_Zg4GulB7VDis2rKeio5Wqg_jmq_SPzp";
